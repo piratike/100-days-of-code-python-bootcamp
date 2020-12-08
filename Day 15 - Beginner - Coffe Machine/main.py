1da0d581fea6d98072768c9e4425468117bec159
Kevin <kevinmachuca96@hotmail.com>
2020-12-08 12:15:19 +0000

    "Created the Day 15 project and added all the "TODO".

---
+++ Day 15 - Beginner - Coffe Machine/main.py
@@ -0,0 +1,9 @@
+# Code for Day 15 Project
+
+# TODO: 1. Prompt user by asking "What would you like?" (espresso/latte/cappuccino).
+# TODO: 2. Turn off the Coffee Machine bu entering "off" to the prompt.
+# TODO: 3. Print report.
+# TODO: 4. Check resources sufficient.
+# TODO: 5. Process coins.
+# TODO: 6. Check transaction succesful.
+# TODO: 7. Make Coffee.
