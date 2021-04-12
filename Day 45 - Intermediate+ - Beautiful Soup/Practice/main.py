fabe7d4822cde45c64fde50b34c84bb1a18b0932
Kevin <kevinmachuca96@hotmail.com>
2021-04-12 20:21:23 +0100

    The project is done, but it has an issue to solve, the titles of the films don't appear in the html obtained from request get

--- Day 45 - Intermediate+ - Beautiful Soup/Practice/main.py
+++ Day 45 - Intermediate+ - Beautiful Soup/Practice/main.py
@@ -1,29 +1,54 @@
 # Code for Day 45 Practice
 
+from os import name
 from bs4 import BeautifulSoup
 
-with open('./Day 45 - Intermediate+ - Beautiful Soup/Practice/website.html', encoding='utf-8') as file:
+# Scraping File -----------------------------------------------------------------------------------
 
-    contents = file.read()
+# with open('./Day 45 - Intermediate+ - Beautiful Soup/Practice/website.html', encoding='utf-8') as file:
 
-soup = BeautifulSoup(contents, 'html.parser')
+#     contents = file.read()
 
-print(soup)
-print(soup.title.name)
-print(soup.title.string)
+# soup = BeautifulSoup(contents, 'html.parser')
 
-all_anchor_tags = soup.find_all(name='a')
-for tag in all_anchor_tags:
-    print(tag.get('href'))
+# print(soup)
+# print(soup.title.name)
+# print(soup.title.string)
 
-class_is_heading = soup.find_all(class_='heading')
-print(class_is_heading)
+# all_anchor_tags = soup.find_all(name='a')
+# for tag in all_anchor_tags:
+#     print(tag.get('href'))
 
-h3_heading = soup.find_all('h3', class_='heading')
-print(h3_heading)
+# class_is_heading = soup.find_all(class_='heading')
+# print(class_is_heading)
 
-name = soup.select_one('#name')
-print(name)
+# h3_heading = soup.find_all('h3', class_='heading')
+# print(h3_heading)
 
-headings = soup.select('.heading')
-print(headings)
+# name = soup.select_one('#name')
+# print(name)
+
+# headings = soup.select('.heading')
+# print(headings)
+
+# Scraping Live Website ---------------------------------------------------------------------------
+
+import requests
+
+yc_webpage = requests.get('https://news.ycombinator.com/news')
+soup = BeautifulSoup(yc_webpage.text, 'html.parser')
+
+articles = soup.find_all(name='a', class_='storylink')
+
+article_texts = []
+article_links = []
+
+for article_tag in articles:
+
+    article_texts.append(article_tag.string)
+    article_links.append(article_tag.get('href'))
+
+article_upvotes = [int(score.getText().split()[0]) for score in soup.find_all(name='span', class_='score')]
+largest_index = article_upvotes.index(max(article_upvotes))
+
+print(article_texts[largest_index], article_links[largest_index])
--- Day 45 - Intermediate+ - Beautiful Soup/Project/main.py
+++ Day 45 - Intermediate+ - Beautiful Soup/Project/main.py
@@ -1,5 +1,20 @@
 # Code for Day 45 Project
 
+import requests
 from bs4 import BeautifulSoup
 
+movies_webpage = requests.get('https://www.empireonline.com/movies/features/best-movies-2/')
+soup = BeautifulSoup(movies_webpage.text, 'html.parser')
+films = soup.find_all(name='div', class_='listicle-item')
 
+titles = []
+
+for film in films:
+
+    titles.append(film.find(name='img').get('alt'))
+
+titles.reverse()
+
+with open('./Day 45 - Intermediate+ - Beautiful Soup/Project/movies.txt', 'w') as file:
+    for movie in titles:
+        file.write(f'{movie}\n')
