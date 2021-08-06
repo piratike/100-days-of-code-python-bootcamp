6b23c1f637826569cd3ce8d2c432385029b9d889
Kevin <kevinmachuca96@hotmail.com>
2021-08-06 19:34:15 +0100

    Day 64 started, I only did 3 requirements

---
+++ Day 64 - Advanced - (Flask) Top 10 Movies Website/main.py
@@ -0,0 +1,71 @@
+from enum import unique
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///new-movies-collection.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+Bootstrap(app)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=True)
+    year = db.Column(db.Integer, nullable=True)
+    description = db.Column(db.String(500), nullable=True)
+    rating = db.Column(db.Float, nullable=True)
+    ranking = db.Column(db.Integer, unique=True, nullable=True)
+    review = db.Column(db.String(250), nullable=True)
+    img_url = db.Column(db.String(250), nullable=True)
+
+    def __repr__(self):
+        return f'<Movie {self.title}'
+
+
+class RateMovieForm(FlaskForm):
+    movie_rating = StringField('Your Rating Out of 10', validators=[DataRequired()])
+    movie_review = StringField('Your Review', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+db.create_all()
+
+
+@app.route("/")
+def home():
+    all_movies = db.session.query(Movie).all()
+    return render_template("index.html", movies=all_movies)
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def edit():
+    movie_form = RateMovieForm()
+    if movie_form.validate_on_submit():
+        movie_to_update = Movie.query.get(request.args.get('movie_id'))
+        movie_to_update.rating = movie_form.movie_rating.data
+        movie_to_update.review = movie_form.movie_review.data
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    return render_template("edit.html", form=movie_form)
+
+
+@app.route("/delete")
+def delete():
+    movie_to_delete = Movie.query.get(request.args.get('movie_id'))
+    db.session.delete(movie_to_delete)
+    db.session.commit()
+
+    return redirect(url_for('home'))
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
